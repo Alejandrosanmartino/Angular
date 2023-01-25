3f40c98fb0e572803e85483422bde0bf084e0aa9
alejandro López <alejandrolopezsanmartino@gmail.com>
2023-01-25 20:04:28 +0100

    se añaden input padre a hijo

---
+++ app-empleados/src/app/empleado-hijo-/empleado-hijo-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-empleado-hijo-component',
+  templateUrl: './empleado-hijo-component.component.html',
+  styleUrls: ['./empleado-hijo-component.component.css']
+})
+export class EmpleadoHijoComponentComponent implements OnInit {
+
+@Input()empleadoDeLista:Empleado; 
+@Input()variableIndice:number;
+ 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
