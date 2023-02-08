5086c12153fa6a8273cd97363d5b8735da17903c
Alejandro López <Alejandro.lopez@altaresp.es>
2023-02-08 12:28:42 +0100

    guardado

---
+++ proyecto004/src/app/dado/dado.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-dado',
+  templateUrl: './dado.component.html',
+  styleUrls: ['./dado.component.css']
+})
+export class DadoComponent {
+
+@Input () valor:string="";
+
+constructor(){
+
+  }
+
+ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+}
